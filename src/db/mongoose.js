b6eb3dad6306bbdab9805d3028ac903db7db5989
Ariye Bar-tov <ariyeb@gmail.com>
2021-03-09 03:57:02 +0200

    fixing mongodb connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB, {
+const user = process.env.MONGODB_USER;
+const password = process.env.MONGODB_PASSWORD;
+const dbName = process.env.MONGODB_DBNAME;
+const mongodbUrl = `mongodb+srv://${user}:${password}@cluster0.jzjfj.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+mongoose.connect(mongodbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
